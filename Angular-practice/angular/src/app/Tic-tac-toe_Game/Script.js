254115f0d1549c7541bdd2459644a7bb15c48d48
Harish Nishad <harish.nishad0775@gmail.com>
2021-11-10 17:46:58 +0530

    Add Ai Player

--- Angular-practice/angular/src/app/Tic-tac-toe_Game/Script.js
+++ Angular-practice/angular/src/app/Tic-tac-toe_Game/Script.js
@@ -11,56 +11,79 @@ const winCombos = [
 	[0, 4, 8],
 	[6, 4, 2]
 ]
+
 const cells = document.querySelectorAll('.cell');
 startGame();
+
 function startGame() {
 	document.querySelector(".endgame").style.display = "none";
 	origBoard = Array.from(Array(9).keys());
 	for (var i = 0; i < cells.length; i++) {
 		cells[i].innerText = '';
 		cells[i].style.removeProperty('background-color');
-		cells[i].addEventListener('click', turnClick);
+		cells[i].addEventListener('click', turnClick, false);
 	}
 }
+
 function turnClick(square) {
-	turn(square.target.id, huPlayer)
+	if (typeof origBoard[square.target.id] == 'number') {
+		turn(square.target.id, huPlayer)
+		if (!checkTie()) turn(bestSpot(), aiPlayer);
+	}
 }
+
 function turn(squareId, player) {
 	origBoard[squareId] = player;
 	document.getElementById(squareId).innerText = player;
-	let gameWon = checkWin(origBoard, player);
-	if (gameWon){
-           gameOver(gameWon);
-    }
+	let gameWon = checkWin(origBoard, player)
+	if (gameWon) gameOver(gameWon)
 }
-function checkWin(board, player) {
-	// let plays = board.reduce((a, e, i) => 
-	// 	(e === player) ? a.concat(i) : a, []);
-    let plays = board.reduce((a,e,i)=>  //play store Array of Index Postion like [1,4,7]
-    (e === player)?a.concat(i) :a,[]);//[] initlize the Accmulator to the Empty Array
-    
-	let gameWon = null;
-    
-    console.log("Plays",plays);
 
-	for (let [index, win] of winCombos.entries()) { // go every element of index&win of wincombos.entries
-      console.log("win" ,win,"Index",index)  // win check Every time is I am Win for the Given index
+function checkWin(board, player) {
+	let plays = board.reduce((a, e, i) => 
+		(e === player) ? a.concat(i) : a, []);
+	let gameWon = null;
+	for (let [index, win] of winCombos.entries()) {
 		if (win.every(elem => plays.indexOf(elem) > -1)) {
-            console.log("win",win);
-            console.log("gameWon",gameWon);
 			gameWon = {index: index, player: player};
 			break;
 		}
 	}
-    console.log("return gameWon",gameWon)
 	return gameWon;
 }
+
 function gameOver(gameWon) {
 	for (let index of winCombos[gameWon.index]) {
 		document.getElementById(index).style.backgroundColor =
-			gameWon.player == huPlayer ? "green" : "red";
+			gameWon.player == huPlayer ? "blue" : "red";
 	}
 	for (var i = 0; i < cells.length; i++) {
 		cells[i].removeEventListener('click', turnClick, false);
 	}
+	declareWinner(gameWon.player == huPlayer ? "You win!" : "You lose.");
+}
+
+function declareWinner(who) {
+	document.querySelector(".endgame").style.display = "block";
+	document.querySelector(".endgame .text").innerText = who;
+}
+
+function emptySquares() {
+	return origBoard.filter(s => typeof s == 'number');
+}
+
+function bestSpot() {
+	return emptySquares()[0];
+}
+
+function checkTie() {
+	if (emptySquares().length == 0) {
+		for (var i = 0; i < cells.length; i++) {
+			cells[i].style.backgroundColor = "green";
+			cells[i].removeEventListener('click', turnClick, false);
+		}
+		declareWinner("Tie Game!")
+		return true;
+	}
+	return false;
 }
